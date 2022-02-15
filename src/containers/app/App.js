86c264118f5ef35f300a3ad4167000f55d950548
Dean <dean@deansantillan.com>
2022-02-15 17:45:28 +1000

    initial commit(): added folder structure base, setup firebase auth app, storage, firestore db and added listing and rules for firebase and firestore.

---
+++ src/containers/app/App.js
@@ -0,0 +1,7 @@
+function App() {
+	return (
+		<div className='App'>Housify - online sales and rental platform.</div>
+	);
+}
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/compat/app';
+import { getFirestore } from 'firebase/compat/firestore';
+//import 'firebase/compat/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCjmzcg4pCLROolq5pyU8a8q_XAiMXszwQ',
+	authDomain: 'housify-devdojo-22.firebaseapp.com',
+	projectId: 'housify-devdojo-22',
+	storageBucket: 'housify-devdojo-22.appspot.com',
+	messagingSenderId: '680844097296',
+	appId: '1:680844097296:web:ca07473a7335714fc35795',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
